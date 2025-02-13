21965b7df1b7da0d4639956da0d5dc1d4b7705d4
Agustin Cabrera <58793527+AgustinCabrera@users.noreply.github.com>
2025-02-13 08:26:45 -0300

    init

---
+++ src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ProjectsModule } from './projects/projects.module';
+import { TasksModule } from './tasks/tasks.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [ProjectsModule, TasksModule, AuthModule, UsersModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
